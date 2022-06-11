90a294f5ae9609cca87cfe805b7a52615c7bcea1
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-11 20:14:45 +0530

    server side processing added //rohit

--- routes/user.js
+++ routes/user.js
@@ -19,14 +19,40 @@ routes.post('/addNewUser', async (req,res)=>{
 
 // -------------------getting all user
 
-routes.get('/getAllUser', async (req,res)=>{
-   try{
-    const data=await User.find()
-    res.status(200).json(data)
-   }catch(err){
-    res.status(500).json(err)
-   }
+// routes.get('/getAllUser', async (req,res)=>{
+//     try{
+//      const data=await User.find()
+//      res.status(200).json(data)
+//     }catch(err){
+//      res.status(500).json(err)
+//     }
+//  })
+
+routes.get('/getAllUser', paginatedResults(), async (req,res)=>{
+    res.json(res.paginatedResults);
 })
+
+function paginatedResults() {
+    return async (req, res, next) => {
+      
+      const page = parseInt(req.query.page);
+      const limit = parseInt(req.query.limit);
+      const skipIndex = (page - 1) * limit;
+      const results = {};
+  
+      try {
+        results.results = await User.find()
+          .sort({ _id: 1 })
+          .limit(limit)
+          .skip(skipIndex)
+          .exec();
+        res.paginatedResults = results;
+        next();
+      } catch (e) {
+        res.status(500).json({ message: "Error Occured" });
+      }
+    };
+  }
 
 //--------------------find user by id
 
@@ -39,6 +65,8 @@ routes.get('/find/:id', async(req,res)=>{
      res.status(500).json(err)
    }
 })
+
+// ----------------------update user
 
 routes.post('/updateUser/:id', async(req,res)=>{
     try{
